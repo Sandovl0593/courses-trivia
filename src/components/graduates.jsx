bbc922e561b00cb6a9fc6daea76a0cf256f1b904
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-03-23 20:57:33 -0500

    styling and fix components props Graduates.jsx

--- src/components/graduates.jsx
+++ src/components/graduates.jsx
@@ -4,85 +4,76 @@ import '../assets/Graduates.css'
 
 export const UserContext = React.createContext();
 
-function ShowPreviewCourses() {
-  const navigate = useNavigate();
-
-  return (
-    <UserContext.Consumer>
-      {
-        value => {
-          <div className='grad-btn'>
-            <h1>{value}</h1> <br/>
-            <h2>Cursos ofrecidos</h2>
-            <button onClick= {
-              () => {  navigate(`/graduates/${value}`); }
-            }>
-              Continuar
-            </button>
-          </div>
-        }
-      }
-    </UserContext.Consumer>
-  )
-}
-
 class Graduates extends React.Component {
 
   constructor(props) {
     super(props);
     this.showPreview = this.showPreview.bind(this);
+    this.closePreview = this.closePreview.bind(this);
     this.state = {
-      showInfoGrads: false
+      indexInfo: "",
+      showInfoGrads: false,
+      closeInfoGrads: true
     };
   }
-
-  // ListGrauates() {
-  //   return (
-  //     <UserContext.Consumer> 
-  //       {
-  //         value => value.map(
-  //           ciclo => <div
-  //             key={ciclo}  id= {ciclo}
-  //             onClick={this.showPreview}>
-  //             <p>Ciclo {ciclo}</p>  
-  //           </div>
-  //         )
-  //       }
-  //     </UserContext.Consumer>
-  //   )
-  // }
-
-  showPreview() {
-    this.setState({showInfoGrads: true});
+  
+  showPreview(ciclo) {
+    this.setState({
+      showInfoGrads: true,
+      closeInfoGrads: false,
+      indexInfo: ciclo
+    });
   }
 
+  closePreview() {
+    this.setState({
+      closeInfoGrads: true,
+      showInfoGrads: false
+    });
+  }
+  
   render() {
-    const ciclos = ["2021-I", "2022-I", "2022-II", "2023-I",
-                    "2023-II", "2024-I", "2024-II", "2025-I",
-                    "2025-II", "2026-I"];
+    const ciclos =  ["2021-I", "2022-I", "2022-II", "2023-I",
+                     "2023-II", "2024-I", "2024-II", "2025-I",
+                     "2025-II", "2026-I"]
     return (
       <div className='Graduates'>
-        <h1 style={{padding: "4em 0 2em 0"}}>
+        <h1 style={{padding: "2.35em 0 2em 0"}}>
           Selecciona un ciclo académico!
         </h1>
 
         <div className='grads-list'> 
           {
-            ciclos.map(
-              ciclo => <div
+            ciclos.map(ciclo => <div
                 key={ciclo}  id= {ciclo}
-                onClick={this.showPreview}>
+                onClick= { () =>{ this.showPreview(ciclo); }}>
                 <p>Ciclo {ciclo}</p>  
               </div>
             )
           }
-          {
-            this.state.showInfoGrads &&
-            <ShowPreviewCourses />
-          }
         </div>
+        {
+          this.state.showInfoGrads && !this.state.closeInfoGrads &&
+          <div className='gradinfo-box'>
+            <div>
+              <button className='X-btn' onClick={this.closePreview}>X</button>
+              <h1>
+                {this.state.indexInfo}
+              </h1> <br/>
+
+              <h2>Cursos ofrecidos</h2>
+              <button onClick= { () => {  
+                  const navigate = useNavigate();
+                  navigate(`/graduates/${value}`);
+                }
+              }>
+                Continuar
+              </button>
+            </div>
+          </div>
+        }
       </div>
-    )
+    );
   }
 }
 
